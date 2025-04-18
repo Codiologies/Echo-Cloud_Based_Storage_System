e6c4ce51cbe9556cea89efb4d39dfb5dcc7ef0f6
SAJAL GUPTA <sajal.gupta2812@gmail.com>
2025-04-19 02:52:51 +0530

    Update settings.py

--- CLOUD_STORAGE/CLOUD_STORAGE/settings.py
+++ CLOUD_STORAGE/CLOUD_STORAGE/settings.py
@@ -146,8 +146,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # For Gmail
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'httpsdesk@gmail.com'  # Replace with your Gmail address
-EMAIL_HOST_PASSWORD = 'vnpy nfly zeyz mphx'  # Replace with your Gmail App Password
-DEFAULT_FROM_EMAIL = 'Echo <httpsdesk@gmail.com>'  # Changed to include display name "Echo"
+EMAIL_HOST_USER = ''  # Replace with your Gmail address
+EMAIL_HOST_PASSWORD = ''  # Replace with your Gmail App Password
+DEFAULT_FROM_EMAIL = ''  # Changed to include display name "Echo"
 
 # ... rest of the existing code ...
